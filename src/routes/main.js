78d7ebc17686bd5b333b9cf4a0dfc5924cccd90e
Otávio Marques <otaviomarques@gmail.com>
2020-07-22 11:56:14 -0300

    lint

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,9 +1,9 @@
-import controller from "./../controllers/main.js";
+import controller from './../controllers/main.js';
 
 export default [
   {
-    method: "GET",
-    path: "/",
+    method: 'GET',
+    path: '/',
     handler: controller.index,
   },
 ];
