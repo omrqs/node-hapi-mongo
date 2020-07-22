c0d63bde8817c40caed17c548576a2bcbe124b92
Otávio Marques <otaviomarques@gmail.com>
2020-07-22 16:51:06 -0300

    composite mongo url by env vars.

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -2,8 +2,25 @@ import Mongoose from 'mongoose';
 
 class Db {
   constructor() {
-    Mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
-    Mongoose.set('useFindAndModify', false)
+    const {
+      MONGO_USERNAME,
+      MONGO_PASSWORD,
+      MONGO_HOST,
+      MONGO_PORT,
+      MONGO_DB
+    } = process.env;
+    
+    const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`;
+
+    Mongoose.connect(url, {
+      retryWrites: true,
+      useNewUrlParser: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 500,
+      connectTimeoutMS: 10000,
+    });
+
+    Mongoose.set('useFindAndModify', false);
   }
 
   init() {
@@ -18,4 +35,4 @@ const db = new Db();
 db.init();
 db.connection.on('error', console.error.bind(console, 'connection error:'));
 
-export default { db };
+export default db;
