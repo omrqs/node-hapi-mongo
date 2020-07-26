8f2c8bf9a5c565869814dfe3f5e5d04ca1384ceb
Otávio Marques <otaviomarques@gmail.com>
2020-07-26 13:27:28 -0300

    include fullname

--- src/schemes/user/new.js
+++ src/schemes/user/new.js
@@ -1,6 +1,7 @@
 import Joi from '@hapi/joi';
 
 export default Joi.object({
+  fullname: Joi.string(),
   email: Joi.string().email().required(),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
 });
