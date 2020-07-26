9fe67ac3e60a56fb1bf5674a6b2c6abbe8c4c79d
Otávio Marques <otaviomarques@gmail.com>
2020-07-26 19:43:27 -0300

    refactor guard

--- src/controllers/guard.js
+++ src/controllers/guard.js
@@ -1,17 +1,25 @@
-import TokenSchema from './../schemes/auth/token.js';
+import Model from './../models/user.js';
 import Joi from '@hapi/joi';
 
 class Guard {
   async cookie(req, h) {
     const sessionCookie = req.state.session || "";
+    const { token } = sessionCookie;
+    const user = await Model.findOneByToken(token);
+
+    if (!user) {
+      return h.response({ message: "unauthorized." }).code(401).takeover();
+    }
 
     return h.response({ message: "authenticated" });
   }
 
   async token(req, h) {
-    const { error } = TokenSchema.validate(req.headers);
-    if (error) {
-      return h.response({ message: error.details }).code(400).takeover();
+    const authorization = req.headers.authorization || '';
+    const user = await Model.findOneByToken(authorization);
+
+    if (!user) {
+      return h.response({ message: "unauthorized." }).code(401).takeover();
     }
 
     return h.response({ message: "authenticated" });
