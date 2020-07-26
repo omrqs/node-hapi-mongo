eeffacc126e6ffaa64dbf34baa7fc5f7ed57442b
Otávio Marques <otaviomarques@gmail.com>
2020-07-26 13:30:31 -0300

    retrieve auth user from db

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,26 +1,38 @@
 import LoginSchema from './../schemes/auth/login.js';
+import Model from './../models/user.js';
 
 class Auth {
   async login(req, h) {
     const { error } = LoginSchema.validate(req.payload);
     if (error) {
-      return h.response({ message: error.details }).code(400).takeover();
+      return h.response({ message: error.details }).code(400);
     }
 
-    const { email } = req.payload;
+    const { email, password } = req.payload;
 
-    return h.response({
-      message: `logged`,
-    })
-    .code(201)
-    .state("session", {
-      email: email
-    });
+    return Model.findOneByUsername(email)
+      .then(async (user) => {
+        const checkPwd = await user.comparePwd(password);
+
+        if (checkPwd) {
+          return h.response({
+            message: `logged`,
+            token: user.token,
+            user: await user.toJson()
+          })
+          .code(201)
+          .state("session", await user.toJson());
+        }
+
+        return h.response({ message: "Invalid credentials." }).code(401);
+      })
+      .catch(err => {
+        console.log("err", err);
+        return h.response({ err: err.getMessage() }).code(500);
+      });
   }
   async logout(req, h) {
-    return h.response({
-      message: `logout`,
-    }).unstate('session');
+    return h.response({ message: `logout` }).unstate('session');
   }
 }
 
