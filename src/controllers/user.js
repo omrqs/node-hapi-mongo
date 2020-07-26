d8fee9c99582b13983c50e0c02f366d36bde81e6
Otávio Marques <otaviomarques@gmail.com>
2020-07-26 13:29:08 -0300

    improvement endpoints

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,6 @@
 import NewSchema from './../schemes/user/new.js';
 import UpdateSchema from './../schemes/user/update.js';
+import PasswordSchema from './../schemes/user/password.js';
 import Model from './../models/user.js';
 import Joi from '@hapi/joi';
 
@@ -10,63 +11,72 @@ class User {
         return h.response({ users });
       })
       .catch(err => {
-        return h.response({ message: err }).code(500).takeover();
+        return h.response({ message: err }).code(500);
       });
   }
 
   async new (req, h) {
     const { error } = NewSchema.validate(req.payload);
     if (error) {
-      return h.response({ message: error.details }).code(400).takeover();
+      return h.response({ message: error.details }).code(400);
     }
 
     return Model.create(req.payload)
-      .then(user => {
+      .then(async (user) => {
         return h.response({
           message: 'Document created.', 
-          user
+          user: await user.toJson()
         });
       })
       .catch(err => {
-        return h.response({ message: err }).code(500).takeover();
+        return h.response({ message: err }).code(500);
       });
   }
 
   async show (req, h) {
     return Model.findById(req.params.id)
-      .then(user => {
-        return h.response({ user });
+      .then(async (user) => {
+        return h.response({ user: await user.toJson() });
       })
       .catch(err => {
-        return h.response({
-          message: "The id doesn't exist in the database."
-        })
-        .code(404)
-        .takeover();
+        return h.response({ message: "The id doesn't exist in the database." }).code(404);
       })
   }
 
   async update (req, h) {
     const { error } = UpdateSchema.validate(req.payload);
     if (error) {
-      return h.response({ message: error.details })
-        .code(400)
-        .takeover();
+      return h.response({ message: error.details }).code(400);
     }
 
     return Model.findByIdAndUpdate(req.params.id, req.payload, { new: true })
-    .then(user => {
+    .then(async (user) => {
       return h.response({
         message: "Document was updated.",
-        user
+        user: await user.toJson()
       });
     })
     .catch(err => {
+      return h.response({ message: "The id doesn't exist in the database." }).code(404);
+    });
+
+  }
+  
+  async changePassword (req, h) {
+    const { error } = PasswordSchema.validate(req.payload);
+    if (error) {
+      return h.response({ message: error.details }).code(400);
+    }
+
+    return Model.findByIdAndUpdate(req.params.id, req.payload, { new: true })
+    .then(async (user) => {
       return h.response({
-        message: "The id doesn't exist in the database."
-      })
-      .code(404)
-      .takeover();
+        message: "Password was updated.",
+        user: await user.toJson()
+      });
+    })
+    .catch(err => {
+      return h.response({ message: "The id doesn't exist in the database." }).code(404);
     });
 
   }
