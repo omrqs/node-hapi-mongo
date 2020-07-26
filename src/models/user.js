35ff316efaf742a8c01d255f96d09037a4dc2b5c
Otávio Marques <otaviomarques@gmail.com>
2020-07-26 19:44:53 -0300

    include roles

--- src/models/user.js
+++ src/models/user.js
@@ -7,7 +7,8 @@ const UserSchema = Db.schema({
   fullname: { type: String },
   email: { type: String, required: true, unique: true, index: true },
   password: { type: String, required: true },
-  token: { type: String, unique: true, index: true }
+  token: { type: String, unique: true, index: true },
+  roles: { type: Array }
 }, { versionKey: false, timestamps: true })
 
 UserSchema.pre('save', function (next) {
@@ -15,6 +16,7 @@ UserSchema.pre('save', function (next) {
     return next();
   }
 
+  this.roles = ["ROLE_USER"];
   this.token = Bcrypt.genSaltSync(SALT_LEVEL);
   this.password = Bcrypt.hashSync(this.password, SALT_LEVEL);
 
@@ -29,13 +31,20 @@ UserSchema.methods.toJson = async function () {
   };
 };
 
+UserSchema.methods.encryptPwd = function (password) {
+  return Bcrypt.hashSync(this.password, SALT_LEVEL);
+};
+
 UserSchema.methods.comparePwd = async function (password) {
-  // return password === this.password;
   return Bcrypt.compare(password, this.password).then(res => res);
 };
 
 UserSchema.statics.findOneByUsername = async function (email) {
   return this.findOne({ email: email });
+};
+
+UserSchema.statics.findOneByToken = async function (token) {
+  return this.findOne({ token: token });
 };
 
 const model = Db.instance.model('User', UserSchema);
